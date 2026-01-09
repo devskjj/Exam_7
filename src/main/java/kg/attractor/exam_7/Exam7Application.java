a0b028afedd7156e8a616d8e611dad3c7c54d733
Denis Eroshevskii <devskjj@gmail.com>
2026-01-10 00:29:31 +0600

    "Init project"

---
+++ src/main/java/kg/attractor/exam_7/Exam7Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.exam_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam7Application.class, args);
+    }
+
+}
